9976cf0f4011b3be618f5282c366df5d4e7e9143
oza6ut0ne <33759728+oza6ut0ne@users.noreply.github.com>
2021-04-26 02:26:21 +0900

    Support Windows partially

--- src/util/windows.rs
+++ src/util/windows.rs
@@ -1,3 +1,17 @@
+use std::ffi::CString;
+use libc::{dup2, fileno, fopen};
+
+const STDERR_FILENO: i32 = 2;
+
 pub fn suppress_stderr() {
-    // nop.
+    let c_str_nul = CString::new("nul").unwrap();
+    let c_str_w = CString::new("w").unwrap();
+
+    let fnul = unsafe { fopen(c_str_nul.as_ptr(), c_str_w.as_ptr()) };
+    if fnul.is_null() {
+        return
+    }
+
+    // FIXME: Not working in PowerShell.
+    unsafe { dup2(fileno(fnul), STDERR_FILENO) };
 }
