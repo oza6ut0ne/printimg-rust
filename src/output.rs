80ec8da362b26cc44d13c2b1a8f635d2fce725d4
oza6ut0ne <33759728+oza6ut0ne@users.noreply.github.com>
2021-12-27 15:12:49 +0900

    Generalize printer

--- src/output.rs
+++ src/output.rs
@@ -1,12 +1,17 @@
+use std::{
+    io::{BufWriter, Write},
+    marker::PhantomData,
+};
+
 use anyhow::Result;
 use opencv::{core, prelude::*};
-use std::io::{BufWriter, StdoutLock, Write};
 
+pub struct PrinterFactory<T> {
+    _marker: PhantomData<fn() -> T>,
+}
 
-pub struct PrinterFactory;
-
-impl PrinterFactory {
-    pub fn create(flat: &bool) -> Box<dyn Printer> {
+impl<T: Write> PrinterFactory<T> {
+    pub fn create(flat: &bool) -> Box<dyn Printer<T>> {
         if *flat {
             Box::new(FlatPrinter)
         } else {
@@ -15,16 +20,14 @@ impl PrinterFactory {
     }
 }
 
-
-pub trait Printer {
-    fn print_img(&self, img: &core::Mat, out: &mut BufWriter<StdoutLock>) -> Result<()>;
+pub trait Printer<T: Write> {
+    fn print_img(&self, img: &core::Mat, out: &mut BufWriter<T>) -> Result<()>;
 }
-
 
 struct FlatPrinter;
 
-impl Printer for FlatPrinter {
-    fn print_img(&self, img: &core::Mat, out: &mut BufWriter<StdoutLock>) -> Result<()> {
+impl<T: Write> Printer<T> for FlatPrinter {
+    fn print_img(&self, img: &core::Mat, out: &mut BufWriter<T>) -> Result<()> {
         let size = img.size()?;
         for y in 0..size.height {
             for x in 0..size.width {
@@ -37,20 +40,19 @@ impl Printer for FlatPrinter {
     }
 }
 
-
 struct SuperResolutionPrinter;
 
-impl Printer for SuperResolutionPrinter {
-    fn print_img(&self, img: &core::Mat, out: &mut BufWriter<StdoutLock>) -> Result<()> {
+impl<T: Write> Printer<T> for SuperResolutionPrinter {
+    fn print_img(&self, img: &core::Mat, out: &mut BufWriter<T>) -> Result<()> {
         let size = img.size()?;
         for y in (0..size.height - 1).step_by(2) {
             for x in 0..size.width {
                 let upper = img.at_2d::<core::Vec3b>(y, x)?;
                 let lower = img.at_2d::<core::Vec3b>(y + 1, x)?;
                 write!(
-                    out, "\x1b[38;2;{};{};{}m\x1b[48;2;{};{};{}m▀",
-                    upper[2], upper[1], upper[0],
-                    lower[2], lower[1], lower[0],
+                    out,
+                    "\x1b[38;2;{};{};{}m\x1b[48;2;{};{};{}m▀",
+                    upper[2], upper[1], upper[0], lower[2], lower[1], lower[0],
                 )?;
             }
             out.write_all(b"\x1b[0m\n")?;
